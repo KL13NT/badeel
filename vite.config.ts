a44e27ea54ec89a0e88881b62d2ca87146ac6cc9
Nabil Tharwat <nabil.tharwat@outlook.com>
2023-11-10 20:30:24 +0200

    ci: chunks config
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,11 @@ import dsv from "@rollup/plugin-dsv";
 
 export default defineConfig({
 	plugins: [solid(), dsv()],
-	assetsInclude: ["**/*.csv"],
 	build: {
 		rollupOptions: {
 			output: {
 				manualChunks: {
 					solid: ["solid-js"],
-					data: ["./src/assets/data.csv"],
 				},
 			},
 		},
