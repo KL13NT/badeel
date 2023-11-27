36498bc83b883cc283b41d2943db9ab3adcddcb8
Nabil Tharwat <nabil.tharwat@outlook.com>
2023-11-27 23:43:36 +0200

    refactor: use search params instead of stores in navigation filter
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- src/components/NavigationFilter/NavigationFilter.tsx
+++ src/components/NavigationFilter/NavigationFilter.tsx
@@ -1,16 +1,15 @@
-import { For, batch, createSignal, onMount } from "solid-js";
+import { For, batch, createSignal } from "solid-js";
 
 import Toggle from "~components/Toggle/Toggle";
 import Button from "~components/Button/Button";
-
-import { useFilterStore } from "~stores/filter";
-import { useFilterQuery } from "~hooks/useFilterQuery";
 
 import { Filter } from "src/types";
 
 import styles from "./NavigationFilter.module.scss";
 
 import ArrowIcon from "~assets/icons/arrow-down.svg?component-solid";
+import { STATUS_FILTER_OPTIONS } from "~constants/filters";
+import { useSearchQuery } from "~hooks/useSearchQuery";
 
 interface Option {
 	key: Filter;
@@ -18,20 +17,17 @@ interface Option {
 }
 
 export default function NavigationFilter() {
-	const [state, setState] = useFilterStore;
 	const [expanded, setExpanded] = createSignal(false);
-	const { status, updateStatus } = useFilterQuery();
+	const { params, updateFilter } = useSearchQuery();
 
 	const activate = (optionKey: Option["key"]) => {
-		const selected = state.options.find((option) => option.key === optionKey)!;
+		const selected = STATUS_FILTER_OPTIONS.find(
+			(option) => option.key === optionKey
+		)!;
 
 		batch(() => {
 			setExpanded(false);
-			updateStatus(selected.key);
-
-			setState({
-				selected,
-			});
+			updateFilter(selected.key);
 		});
 	};
 
@@ -39,27 +35,23 @@ export default function NavigationFilter() {
 		setExpanded(updated);
 	};
 
-	onMount(() => {
-		if (status) {
-			activate(status);
-		}
-	});
-
 	const rest = () =>
-		state.options.filter((option) => option.key !== state.selected.key);
+		STATUS_FILTER_OPTIONS.filter((option) => option.key !== params.status);
+	const selected = () =>
+		STATUS_FILTER_OPTIONS.find((option) => option.key === params.status)!;
 
 	return (
 		<div class={styles.navigationFilter}>
 			<p class="t-body">انت الآن تُظهر</p>
 
 			<Toggle
-				label={state.selected.title}
+				label={selected().title}
 				onChange={toggleMenu}
 				aria-haspopup="menu"
 				pressed={expanded()}
 				class={styles.toggle}
 			>
-				{state.selected.title}
+				{selected().title}
 
 				<ArrowIcon stroke="currentColor" />
 			</Toggle>
