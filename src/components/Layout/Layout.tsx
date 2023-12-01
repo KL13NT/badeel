75e5a00ee853951c180ec2c70ab8272d68704ea7
Nabil Tharwat <nabil.tharwat@outlook.com>
2023-12-01 20:57:58 +0200

    feat: hide sticky navbar on scroll
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -10,15 +10,72 @@ import styles from "./Layout.module.scss";
 
 import logo from "~assets/icons/logo.svg";
 import t from "~utils/messages";
+import { onMount } from "solid-js";
 
 interface Props {
 	children: JSX.Element;
 }
 
+let headerRef: HTMLDivElement | undefined;
+
 export default function Layout(props: Props) {
+	onMount(() => {
+		let lastScrollTop =
+			window?.scrollY || document?.documentElement?.scrollTop || 0;
+		let visible = true;
+
+		const getViewportWidth = () => {
+			return Math.max(
+				document.documentElement.clientWidth || 0,
+				window.innerWidth || 0
+			);
+		};
+
+		const hide = () => {
+			headerRef!.style.transform = "translateY(-100%)";
+
+			visible = false;
+		};
+
+		const show = () => {
+			headerRef!.style.transform = "translateY(0)";
+
+			visible = true;
+		};
+
+		const handleScroll = () => {
+			const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+			if (scrollTop < lastScrollTop && !visible) {
+				show();
+			} else if (scrollTop > 120 && scrollTop > lastScrollTop && visible) {
+				hide();
+			}
+
+			lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Negative scrolling
+		};
+
+		if (getViewportWidth() <= 700) {
+			document.addEventListener("scroll", handleScroll, false);
+		} else {
+			headerRef!.style.position = "relative";
+		}
+
+		window.addEventListener("resize", () => {
+			if (getViewportWidth() <= 700) {
+				document.addEventListener("scroll", handleScroll, false);
+				headerRef!.style.position = "fixed";
+			} else {
+				show();
+				headerRef!.style.position = "relative";
+				document.removeEventListener("scroll", handleScroll, false);
+			}
+		});
+	});
+
 	return (
 		<>
-			<nav class={styles.navbar}>
+			<nav class={styles.navbar} ref={headerRef}>
 				<div class={styles.upper}>
 					<div class={styles.right}>
 						<img src={logo} class={styles.logo} alt="" role="presentation" />
