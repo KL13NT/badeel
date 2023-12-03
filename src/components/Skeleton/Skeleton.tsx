c1d3e13b5320e3af8d90cec4dd32ef3747a6147d
Nabil Tharwat <nabil.tharwat@outlook.com>
2023-12-03 16:01:11 +0200

    refactor: skeleton to accept more props
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -1,19 +1,28 @@
 import clsx from "clsx";
 import { JSX } from "solid-js/jsx-runtime";
-import { mergeProps } from "solid-js";
+import { mergeProps, splitProps } from "solid-js";
 import { Dynamic } from "solid-js/web";
 
 import styles from "./Skeleton.module.scss";
 
-interface Props {
+interface Props extends JSX.AriaAttributes {
+	[key: string]: unknown;
 	class?: string;
 	as?: keyof JSX.HTMLElementTags;
+	children?: JSX.Element;
 }
 
 export default function Skeleton(_props: Props) {
-	const props = mergeProps({ as: "div" }, _props);
+	const merged = mergeProps({ as: "div" }, _props);
+	const [local, rest] = splitProps(merged, ["as", "class", "children"]);
 
 	return (
-		<Dynamic component={props.as} class={clsx(styles.skeleton, props.class)} />
+		<Dynamic
+			component={local.as}
+			class={clsx(styles.skeleton, local.class)}
+			{...rest}
+		>
+			{local.children}
+		</Dynamic>
 	);
 }
