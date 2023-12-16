814237d777a6f2867cc8015776fad709bf96b929
Nabil Tharwat <nabil.tharwat@outlook.com>
2023-12-16 20:18:39 +0200

    feat: new icon
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- vite.config.ts
+++ vite.config.ts
@@ -138,8 +138,10 @@ export default defineConfig({
 			},
 			includeAssets: [
 				"favicon.ico",
-				"apple-touch-icon.png",
-				"mask-icon.svg",
+				"favicon.svg",
+				"icons/",
+				"29LTAdaFlat-Black.ttf",
+				"29LTAdaFlat-Bold.ttf",
 				"manifest.webmanifest",
 				"og.png",
 			],
@@ -153,44 +155,164 @@ export default defineConfig({
 				orientation: "portrait",
 				icons: [
 					{
-						src: "/icons/android-36x36.png",
-						sizes: "36x36",
+						src: "/icons/android/android-launchericon-512-512.png",
 						type: "image/png",
-					},
-					{
-						src: "/icons/android-48x48.png",
-						sizes: "48x48",
-						type: "image/png",
-					},
-					{
-						src: "/icons/android-72x72.png",
-						sizes: "72x72",
-						type: "image/png",
-					},
-					{
-						src: "/icons/android-96x96.png",
-						sizes: "96x96",
-						type: "image/png",
-					},
-					{
-						src: "/icons/android-144x144.png",
-						sizes: "144x144",
-						type: "image/png",
-					},
-					{
-						src: "/icons/android-chrome-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-					},
-					{
-						src: "/icons/android-384x384.png",
-						sizes: "384x384",
-						type: "image/png",
-					},
-					{
-						src: "/icons/android-chrome-512x512.png",
 						sizes: "512x512",
+					},
+					{
+						src: "/icons/android/android-launchericon-192-192.png",
 						type: "image/png",
+						sizes: "192x192",
+					},
+					{
+						src: "/icons/android/android-launchericon-144-144.png",
+						type: "image/png",
+						sizes: "144x144",
+					},
+					{
+						src: "/icons/android/android-launchericon-96-96.png",
+						type: "image/png",
+						sizes: "96x96",
+					},
+					{
+						src: "/icons/android/android-launchericon-72-72.png",
+						type: "image/png",
+						sizes: "72x72",
+					},
+					{
+						src: "/icons/android/android-launchericon-48-48.png",
+						type: "image/png",
+						sizes: "48x48",
+					},
+					{
+						src: "/icons/ios/16.png",
+						type: "image/png",
+						sizes: "16x16",
+					},
+					{
+						src: "/icons/ios/20.png",
+						type: "image/png",
+						sizes: "20x20",
+					},
+					{
+						src: "/icons/ios/29.png",
+						type: "image/png",
+						sizes: "29x29",
+					},
+					{
+						src: "/icons/ios/32.png",
+						type: "image/png",
+						sizes: "32x32",
+					},
+					{
+						src: "/icons/ios/40.png",
+						type: "image/png",
+						sizes: "40x40",
+					},
+					{
+						src: "/icons/ios/50.png",
+						type: "image/png",
+						sizes: "50x50",
+					},
+					{
+						src: "/icons/ios/57.png",
+						type: "image/png",
+						sizes: "57x57",
+					},
+					{
+						src: "/icons/ios/58.png",
+						type: "image/png",
+						sizes: "58x58",
+					},
+					{
+						src: "/icons/ios/60.png",
+						type: "image/png",
+						sizes: "60x60",
+					},
+					{
+						src: "/icons/ios/64.png",
+						type: "image/png",
+						sizes: "64x64",
+					},
+					{
+						src: "/icons/ios/72.png",
+						type: "image/png",
+						sizes: "72x72",
+					},
+					{
+						src: "/icons/ios/76.png",
+						type: "image/png",
+						sizes: "76x76",
+					},
+					{
+						src: "/icons/ios/80.png",
+						type: "image/png",
+						sizes: "80x80",
+					},
+					{
+						src: "/icons/ios/87.png",
+						type: "image/png",
+						sizes: "87x87",
+					},
+					{
+						src: "/icons/ios/100.png",
+						type: "image/png",
+						sizes: "100x100",
+					},
+					{
+						src: "/icons/ios/114.png",
+						type: "image/png",
+						sizes: "114x114",
+					},
+					{
+						src: "/icons/ios/120.png",
+						type: "image/png",
+						sizes: "120x120",
+					},
+					{
+						src: "/icons/ios/128.png",
+						type: "image/png",
+						sizes: "128x128",
+					},
+					{
+						src: "/icons/ios/144.png",
+						type: "image/png",
+						sizes: "144x144",
+					},
+					{
+						src: "/icons/ios/152.png",
+						type: "image/png",
+						sizes: "152x152",
+					},
+					{
+						src: "/icons/ios/167.png",
+						type: "image/png",
+						sizes: "167x167",
+					},
+					{
+						src: "/icons/ios/180.png",
+						type: "image/png",
+						sizes: "180x180",
+					},
+					{
+						src: "/icons/ios/192.png",
+						type: "image/png",
+						sizes: "192x192",
+					},
+					{
+						src: "/icons/ios/256.png",
+						type: "image/png",
+						sizes: "256x256",
+					},
+					{
+						src: "/icons/ios/512.png",
+						type: "image/png",
+						sizes: "512x512",
+					},
+					{
+						src: "/icons/ios/1024.png",
+						type: "image/png",
+						sizes: "1024x1024",
 					},
 				],
 			},
