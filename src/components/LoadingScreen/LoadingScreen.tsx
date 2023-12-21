d028f42ef3b11ddbf86c824375ee93e0c6817e84
Karim Shalapy <karimshalapy@gmail.com>
2023-12-22 01:35:51 +0200

    feat: animate a palestine map loader

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,17 +1,13 @@
 import styles from "./LoadingScreen.module.scss";
 
+import Palestine from "~assets/icons/palestine.svg?component-solid";
+
 export default function LoadingScreen() {
 	return (
 		<div class={styles.loading} role="alert" aria-live="assertive">
 			<div>
-				<h1>يجري التحميل</h1>
-
-				<div class={styles.spinner}>
-					<div />
-					<div />
-					<div />
-					<div />
-				</div>
+				<Palestine />
+				<h1>جارِ التحميل</h1>
 			</div>
 		</div>
 	);
