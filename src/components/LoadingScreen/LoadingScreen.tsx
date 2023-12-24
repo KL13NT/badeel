9b237a83bc5d62fc592280599e1388c62aee8283
Nabil Tharwat <nabil.tharwat@outlook.com>
2023-12-24 19:03:15 +0200

    fix: svg rendering on loading
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,12 +1,22 @@
+import { onCleanup, onMount } from "solid-js";
+
 import styles from "./LoadingScreen.module.scss";
 
-import Palestine from "~assets/icons/palestine.svg?component-solid";
+import PalestineIcon from "~assets/icons/palestine.svg?component-solid";
 
 export default function LoadingScreen() {
+	onMount(() => {
+		document.querySelector("html")!.style.overflow = "hidden";
+	});
+
+	onCleanup(() => {
+		document.querySelector("html")!.style.overflow = "unset";
+	});
+
 	return (
 		<div class={styles.loading} role="alert" aria-live="assertive">
 			<div>
-				<Palestine />
+				<PalestineIcon />
 				<h1>جارِ التحميل</h1>
 			</div>
 		</div>
