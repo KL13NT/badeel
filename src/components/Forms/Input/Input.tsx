1b32205396117c5be2ddf079ee58d57e8e22822b
Nabil Tharwat <nabil.tharwat@outlook.com>
2024-01-10 20:50:15 +0200

    fix: input width issues on smaller screens
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- src/components/Forms/Input/Input.tsx
+++ src/components/Forms/Input/Input.tsx
@@ -1,7 +1,7 @@
 import { JSX, mergeProps, splitProps } from "solid-js";
+import { Dynamic } from "solid-js/web";
 
 import styles from "./Input.module.scss";
-import { Dynamic } from "solid-js/web";
 
 interface InputProps extends JSX.InputHTMLAttributes<HTMLInputElement> {
 	label: string;
