109357de6bef59e0f4e446b2125fae8531e6d422
Nabil Tharwat <nabil.tharwat@outlook.com>
2024-01-18 22:38:38 +0200

    feat: website tour
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

---
+++ src/components/Tour/Tour.tsx
@@ -0,0 +1,89 @@
+import { driver } from "driver.js";
+import { createEffect } from "solid-js";
+
+import { hasOverlay } from "~stores/overlay";
+
+import "driver.js/dist/driver.css";
+import styles from "./Tour.module.scss";
+
+const tour = driver({
+	showProgress: false,
+
+	showButtons: ["next", "close", "previous"],
+	popoverClass: styles.tour,
+	progressText: `{{current}} من {{total}}`,
+	nextBtnText: "التالي",
+	prevBtnText: "السابق",
+	doneBtnText: "إنهاء",
+
+	onDestroyStarted: () => {
+		if (!tour.hasNextStep() || confirm("هل انت متأكد من إغلاق الرحلة؟")) {
+			tour.destroy();
+		}
+	},
+
+	steps: [
+		{
+			element: undefined,
+			popover: {
+				title: "رحلة التشغيل",
+				description:
+					"مرحباً بكم في بَدِيل. إذا أردتم إلغاء الرحلة قوموا بالضغط على علامة x أعلاه.",
+				side: "bottom",
+				align: "center",
+			},
+		},
+		{
+			element: "[data-tour-id='search']",
+			popover: {
+				title: "البحث",
+				description:
+					"يمكنكم البحث عن أي مُنتج أو شركة باللغتين العربية والإنجليزية من خلال خانة البحث",
+				side: "bottom",
+				align: "center",
+			},
+		},
+		{
+			element: "[data-tour-id='filters']",
+			popover: {
+				title: "الإقسام والترتيب",
+				description:
+					"يمكنكم تحديد أقسام بعينها وتغيير ترتيب المُنتجات في قائمة العرض",
+				side: "bottom",
+				align: "center",
+			},
+		},
+		{
+			element: "[data-tour-id='header']",
+			popover: {
+				title: "يمكنكم مساعدتنا!",
+				description:
+					"إذا أردتم المساعدة يمكنكم ذلك من خلال إضافة المنتجات او إرسال اقتراحاتكم الينا!",
+				side: "bottom",
+				align: "center",
+			},
+		},
+		{
+			popover: {
+				title: "نهاية الرحلة 🥳",
+				description:
+					"وبذلك تكون رحلتنا انتهت, يمكنكم الوصول لصفحة الأسئلة والأجوبة أسفل الموقع.",
+				side: "bottom",
+				align: "center",
+			},
+		},
+	],
+});
+
+export default function Tour() {
+	createEffect(() => {
+		const driven = localStorage.getItem("tour-done") && !import.meta.env.DEV;
+
+		if (!hasOverlay() && !driven) {
+			tour.drive(2);
+			localStorage.setItem("tour-done", String(true));
+		}
+	});
+
+	return <></>;
+}
